dcbc8faf5be3a19b7829f458e2029427bb460692
Anggita Denavia <anggitadenav@gmail.com>
2020-11-25 19:39:02 +0700

    Add Template

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,103 @@
+    <!-- Left Sidebar - style you can find in sidebar.scss  -->
+    <!-- ============================================================== -->
+    <aside class="left-sidebar" data-sidebarbg="skin6">
+        <!-- Sidebar scroll-->
+        <div class="scroll-sidebar" data-sidebarbg="skin6">
+            <!-- Sidebar navigation-->
+            <nav class="sidebar-nav">
+                <ul id="sidebarnav">
+                    <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="index.html"
+                            aria-expanded="false"><i data-feather="home" class="feather-icon"></i><span
+                                class="hide-menu">Dashboard</span>
+                        </a>
+                    </li>
+                    <li class="sidebar-item"> <a class="sidebar-link sidebar-link" id="admin-menu" href="#"
+                            aria-expanded="false"><i data-feather="home" class="feather-icon"></i><span
+                                class="hide-menu">Data Admin</span>
+                        </a>
+                    </li>
+                    <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="index.html"
+                            aria-expanded="false"><i data-feather="home" class="feather-icon"></i><span
+                                class="hide-menu">Ubah Password</span>
+                        </a>
+                    </li>
+                    <li class="list-divider"></li>
+                    <li class="nav-small-cap"><span class="hide-menu">Tentang Desa</span></li>
+
+                    <li class="sidebar-item"> <a class="sidebar-link" href="ticket-list.html" aria-expanded="false"><i
+                                data-feather="tag" class="feather-icon"></i><span class="hide-menu">Profil Desa
+                            </span></a>
+                    </li>
+                    <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="app-chat.html"
+                            aria-expanded="false"><i data-feather="message-square" class="feather-icon"></i><span
+                                class="hide-menu">Visi Misi</span></a></li>
+                    <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="app-calendar.html"
+                            aria-expanded="false"><i data-feather="calendar" class="feather-icon"></i><span
+                                class="hide-menu">Struktur Organisasi</span></a></li>
+                    <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="app-calendar.html"
+                            aria-expanded="false"><i data-feather="calendar" class="feather-icon"></i><span
+                                class="hide-menu">Profil Pedukuhan</span></a></li>
+                    <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="app-calendar.html"
+                            aria-expanded="false"><i data-feather="calendar" class="feather-icon"></i><span
+                                class="hide-menu">Info Desa</span></a></li>
+
+                    <li class="list-divider"></li>
+                    <li class="nav-small-cap"><span class="hide-menu">Layanan dan Aduan</span></li>
+                    <li class="sidebar-item"> <a class="sidebar-link has-arrow" href="javascript:void(0)"
+                            aria-expanded="false"><i data-feather="file-text" class="feather-icon"></i><span
+                                class="hide-menu">Info Layanan </span></a>
+                        <ul aria-expanded="false" class="collapse  first-level base-level-line">
+                            <li class="sidebar-item"><a href="form-inputs.html" class="sidebar-link"><span
+                                        class="hide-menu"> Layanan A
+                                    </span></a>
+                            </li>
+                            <li class="sidebar-item"><a href="form-input-grid.html" class="sidebar-link"><span
+                                        class="hide-menu">Layanan B
+                                    </span></a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="sidebar-item"> <a class="sidebar-link has-arrow" href="javascript:void(0)"
+                            aria-expanded="false"><i data-feather="grid" class="feather-icon"></i><span
+                                class="hide-menu">Layanan </span></a>
+                        <ul aria-expanded="false" class="collapse  first-level base-level-line">
+                            <li class="sidebar-item"><a href="table-basic.html" class="sidebar-link"><span
+                                        class="hide-menu"> Layanan A
+                                    </span></a>
+                            </li>
+                            <li class="sidebar-item"><a href="table-dark-basic.html" class="sidebar-link"><span
+                                        class="hide-menu"> Layanan B
+                                    </span></a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="sidebar-item"> <a class="sidebar-link has-arrow" href="javascript:void(0)"
+                            aria-expanded="false"><i data-feather="bar-chart" class="feather-icon"></i><span
+                                class="hide-menu">Aduan </span></a>
+                    </li>
+                    <li class="list-divider"></li>
+                    <li class="nav-small-cap"><span class="hide-menu">Laporan</span></li>
+
+                    <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="authentication-login1.html"
+                            aria-expanded="false"><i data-feather="lock" class="feather-icon"></i><span
+                                class="hide-menu">Data Penduduk
+                            </span></a>
+                    </li>
+                    <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="authentication-login1.html"
+                            aria-expanded="false"><i data-feather="lock" class="feather-icon"></i><span
+                                class="hide-menu">Statistik Data Layanan
+                            </span></a>
+                    </li>
+                    <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="authentication-register1.html"
+                            aria-expanded="false"><i data-feather="lock" class="feather-icon"></i><span
+                                class="hide-menu">Statistik Data Aduan
+                            </span></a>
+                    </li>
+                </ul>
+            </nav>
+            <!-- End Sidebar navigation -->
+        </div>
+        <!-- End Sidebar scroll-->
+    </aside>
+    <!-- ============================================================== -->
+    <!-- End Left Sidebar - style you can find in sidebar.scss  -->
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return view('index');
 });
+
+Route::get('home', function () {
+    return view('dashboard');
+});
+Route::get('/admin', 'AdminController@index');
