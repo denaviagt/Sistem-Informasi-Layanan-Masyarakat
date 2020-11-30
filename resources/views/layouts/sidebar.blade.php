2efa2a7bbf13e84544f62d289758f5c8b1ee2995
Anggita Denavia <anggitadenav@gmail.com>
2020-12-01 06:44:26 +0700

    Add profil kalurahan

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -16,27 +16,14 @@
                                 class="feather-icon"></i><span class="hide-menu">Data Admin</span>
                         </a>
                     </li>
-                    <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="index.html"
-                            aria-expanded="false"><i data-feather="home" class="feather-icon"></i><span
-                                class="hide-menu">Ubah Password</span>
-                        </a>
-                    </li>
                     <li class="list-divider"></li>
                     <li class="nav-small-cap"><span class="hide-menu">Tentang Desa</span></li>
 
-                    <li class="sidebar-item"> <a class="sidebar-link" href="ticket-list.html" aria-expanded="false"><i
-                                data-feather="tag" class="feather-icon"></i><span class="hide-menu">Profil Desa
+                    <li class="sidebar-item"> <a class="sidebar-link"
+                            href="{{ url('/profil-kalurahan') }}"" aria-expanded=" false"><i data-feather="tag"
+                                class="feather-icon"></i><span class="hide-menu">Profil Desa
                             </span></a>
                     </li>
-                    <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="app-chat.html"
-                            aria-expanded="false"><i data-feather="message-square" class="feather-icon"></i><span
-                                class="hide-menu">Visi Misi</span></a></li>
-                    <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="app-calendar.html"
-                            aria-expanded="false"><i data-feather="calendar" class="feather-icon"></i><span
-                                class="hide-menu">Struktur Organisasi</span></a></li>
-                    <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="app-calendar.html"
-                            aria-expanded="false"><i data-feather="calendar" class="feather-icon"></i><span
-                                class="hide-menu">Profil Pedukuhan</span></a></li>
                     <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="app-calendar.html"
                             aria-expanded="false"><i data-feather="calendar" class="feather-icon"></i><span
                                 class="hide-menu">Info Desa</span></a></li>
