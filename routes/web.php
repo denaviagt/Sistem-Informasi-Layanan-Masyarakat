fbf5d4d6c54ae7b6bbce7bd8f269f84be008eb40
Anggita Denavia <anggitadenav@gmail.com>
2020-12-01 00:04:24 +0000

    Ubah Password and Profil Kalurahan

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,9 @@ Route::get('/', function () {
 Route::get('/admin', function () {
     return view('admin');
 });
+Route::get('/profil-kalurahan', function () {
+    return view('profil-kalurahan');
+});
+Route::get('/edit-password', function () {
+    return view('edit-password');
+});
