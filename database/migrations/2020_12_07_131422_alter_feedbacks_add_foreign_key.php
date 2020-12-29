dd21f183cb18d0512b00c5cd4cb9b4a5a029b0e6
Anggita Denavia <anggitadenav@gmail.com>
2020-12-29 07:09:55 +0000

    Migration and Seeder

---
+++ database/migrations/2020_12_07_131422_alter_feedbacks_add_foreign_key.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterFeedbacksAddForeignKey extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('feedbacks', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('feedback_dusun_id')->constrained('dusuns');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('feedbacks', function (Blueprint $table) {
+            $table->dropForeign(['user_id', 'feedback_dusun_id']);
+        });
+    }
+}
---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+use Faker\Factory as Faker;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create('id_ID');
+        // $gender = $faker->randomElement(['male', 'female']);
+        for ($i = 1; $i <= 5; $i++) {
+            DB::table('admins')->insert([
+                'full_name' => $faker->name,
+                'gender' => $faker->randomElement(['male', 'female']),
+                'email' => $faker->freeEmail,
+                'username' => $faker->userName,
+                'password' => Hash::make('password'),
+                'is_active' => 1,
+                'level' => 'admin',
+            ]);
+        }
+    }
+}
