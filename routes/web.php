7c2c114b0998952fa82ce09bb194b2735d530a6c
devry12 <devrykawiryan@gmail.com>
2021-01-03 19:52:06 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfilKalurahanController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,6 +28,8 @@ Route::get('login', function () {
 Route::get('/admin', 'AdminController@index');
 Route::post('/admin', 'AdminController@store');
 Route::get('/admin/{id}/edit', 'AdminController@edit');
+Route::post('/admin/edit', 'AdminController@update');
+
 Route::get('/profil-kalurahan', 'ProfilKalurahanController@index');
 Route::get('/edit-password', function () {
     return view('edit-password');
