156f38f94f4524b48ffa65d3d30a51addb9624a0
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-01-14 20:27:40 +0700

    tambah layout modal edit password

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -84,6 +84,9 @@
                          <a class="dropdown-item" href="javascript:void(0)"><i data-feather="settings"
                                  class="svg-icon mr-2 ml-1"></i>
                              Account Setting</a>
+                         <a class="dropdown-item" href="#modal-edit-password" data-toggle="modal"><i data-feather="settings"
+                                 class="svg-icon mr-2 ml-1"></i>
+                             Edit Password</a>
                          <div class="dropdown-divider"></div>
                          <a class="dropdown-item" href="{{ route('logout') }}"><i data-feather="power"
                                  class="svg-icon mr-2 ml-1"></i>
@@ -100,5 +103,41 @@
          </div>
      </nav>
  </header>
+ <!--  Modal content for the above example -->
+ <div class="modal fade" id="modal-edit-password" tabindex="-1" role="dialog"
+      aria-labelledby="editPasswordModal" aria-hidden="true">
+     <div class="modal-dialog modal-dialog-centered modal-lg">
+         <div class="modal-content">
+             <form action="#">
+                 <div class="modal-header modal-colored-header bg-primary">
+                     <h4 class="modal-title" id="editPasswordModal">Edit Password</h4>
+                     <button type="button" class="close" data-dismiss="modal"
+                             aria-hidden="true">×</button>
+                 </div>
+                 <div class="modal-body">
+                     <div class="form-group row">
+                         <label for="inputPassword" class="col-sm-3 col-form-label">Password Lama</label>
+                         <div class="col-sm-9">
+                             <input type="password" class="form-control" id="inputPassword" placeholder="Masukkan password lama anda yang ingin dirubah"  required="">
+                         </div>
+                     </div>
+
+                     <div class="form-group row">
+                         <label for="newPassword" class="col-sm-3 col-form-label">Password Baru</label>
+                         <div class="col-sm-9">
+                             <input type="password" class="form-control" id="newPassword" placeholder="Masukkan password baru anda" required="">
+                         </div>
+                     </div>
+
+                     <div class="form-group text-center">
+                         <button class="btn btn-danger m-2" type="reset" data-dismiss="modal">Batal</button>
+                         <button class="btn btn-primary m-2" type="submit">Ubah Password</button>
+                     </div>
+
+                 </div>
+             </form>
+         </div><!-- /.modal-content -->
+     </div><!-- /.modal-dialog -->
+ </div><!-- /.modal -->
  <!-- ============================================================== -->
  <!-- End Topbar header -->
