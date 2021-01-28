669cea9ce84fe45aabef72f736720fe5603d354a
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-01-28 17:44:55 +0700

    menambahkan route api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController\AuthApiController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::prefix('user')->group(function (){
+    Route::prefix('auth')->group(function (){
+        Route::post('login', [AuthApiController::class, 'login']);
+        Route::post('register', [AuthApiController::class, 'register']);
+    });
 });
