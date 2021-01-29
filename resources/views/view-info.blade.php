ba781764ffbb6fdded69bf4bcc0340554be4e922
gilang90 <gilang.ramadhan@students.amikom.ac.id>
2021-01-29 16:20:37 +0700

    View Info Desa view-info

---
+++ resources/views/view-info.blade.php
@@ -0,0 +1,260 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Info Desa Jogotirto</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+
+    <!-- CSS here -->
+    <link href="{{ asset('assets/libs/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('dist/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('dist/css/flaticons.css') }}">
+    <link rel="stylesheet" href="{{ asset('dist/css/slicknav.css') }}">
+    <link rel="stylesheet" href="{{ asset('dist/css/animate.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('dist/css/magnific-popup.css') }}">
+    {{--
+    <link href="{{ asset('dist/css/icons/font-awesome/css/fontawesome.css') }}" rel="stylesheet">
+    --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/fontawesome.min.css" integrity="sha512-shT5e46zNSD6lt4dlJHb+7LoUko9QZXTGlmWWx0qjI9UhQrElRb+Q5DM7SVte9G9ZNmovz2qIaV7IWv0xQkBkw==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="{{ asset('dist/css/themify-icon.css') }}">
+    <link rel="stylesheet" href="{{ asset('dist/css/slick.css') }}">
+    <link rel="stylesheet" href="{{ asset('dist/css/nice-select.css') }}">
+    <link rel="stylesheet" href="{{ asset('dist/css/landingcss.css') }}">
+    <style>
+        .natural-resource-img img {
+            width: 100%
+        }
+
+    </style>
+</head>
+
+<body>
+
+    <!-- Preloader Start -->
+    {{-- <div id="preloader-active">
+        <div class="preloader d-flex align-items-center justify-content-center">
+            <div class="preloader-inner position-relative">
+                <div class="preloader-circle"></div>
+                <div class="preloader-img pere-text">
+                    <img src="assets/img/logo/logo.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div> --}}
+    <!-- Preloader Start -->
+    <!-- Slider Area Start-->
+    <!-- Section-tittle -->
+    <div class="row d-flex justify-content-center">
+        <div class="col-lg-8">
+            <div class="section-tittle text-center mb-80">
+                <h2>Lava Bantal​</h2>
+            </div>
+        </div>
+    </div>
+    <!-- Slider Area End-->
+    <!--================Blog Area =================-->
+    <section class="blog_area single-post-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 posts-list">
+                    <div class="single-post">
+                        <div class="feature-img">
+                            <img class="img-fluid" src="https://www.kartanesia.com/wp-content/uploads/2019/04/Lava-Bantal-Berbah-1.png" alt="">
+                        </div>
+                        <div class="blog_details">
+                            <h2>Daya Tarik dan Keunikan Lava Bantal Berbah
+                            </h2>
+                            <ul class="blog-info-link mt-3 mb-4">
+                                <li><a href="#"><i class="ti-user"></i> Layla </a></li>
+                                <li><a href="#"><i class="ti-comment-alt"></i>January, 29 2020</a></li>
+                            </ul>
+                            <p class="excert">
+                                Meskipun baru dibuka pada tanggal 30 Mei 2016 lalu, tempat wisata ini cukup hits dan hampir tak pernah sepi dari pengunjung setiap harinya. Daya tariknya berupa pemandangan indah nan unik dengan udara yang sangat sejuk di sekitarnya menjadikan tempat wisata ini cukup banyak diminati para wisatawan. Para wisatawan yang datang akan dimanjakan dengan pemandangan jernihnya air sungai yang diapit bebatuan besar di kedua sisinya.
+                            </p>
+                            <p>
+                                Pengunjung akan dapat menyaksikan keindahan embung Tegaltirto yang lokasinya tidak jauh sekitar 500 meter di selatan Lava Bantal Sleman. Jika berada di sekitar sini harus tetap berhati-hati karena debit aliran sungainya cukup deras. Tempat wisata yang termasuk dalam salah satu kawasan Geoheritage ini masih sangat terjaga keasriannya dengan jernihnya air yang berwarna hijau tosca, pepohonan di sekitarnya, dan batuannya yang unik.
+                            </p>
+                            <p>
+                                Harga Tiket Masuk Objek Wisata Lava Bantal Berbah
+                            </p>
+                            <p>
+                                Lava Bantal Berbah merupakan tempat wisata pilihan keluarga yang asyik dan murah untuk menikmati keindahan dan keasrian alam di sekitar Gunung Merapi. Pengunjung yang datang tidak akan dikenakan biaya tiket masuk alias gratis. Mereka cukup membayar biaya parkir kendaraan saja dan biaya tambahan jika ingin bermain wahana yang terdapat di sini.
+
+
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="blog_right_sidebar">
+                        <aside class="single_sidebar_widget popular_post_widget">
+                            <h3 class="widget_title">Recent Post</h3>
+                            <div class="media post_item">
+                                <img src="https://www.yogyes.com/en/yogyakarta-tourism-object/other/malioboro/1.jpg" width="75px" alt="post">
+                                <div class="media-body">
+                                    <a href="https://www.yogyes.com/id/yogyakarta-tourism-object/other/malioboro/">
+                                        <h3>Malioboro Yogyakarta</h3>
+                                    </a>
+                                    <p>January 12, 2019</p>
+                                </div>
+                            </div>
+                            <div class="media post_item">
+                                <img src="https://www.yogyes.com/en/yogyakarta-tourism-object/other/alun-alun-kidul/1.jpg" width="75px" alt="post">
+                                <div class="media-body">
+                                    <a href="https://www.yogyes.com/id/yogyakarta-tourism-object/other/alun-alun-kidul/">
+                                        <h3>Alun-Alun Kidul</h3>
+                                    </a>
+                                    <p>02 Hours ago</p>
+                                </div>
+                            </div>
+                            <div class="media post_item">
+                                <img src="https://www.yogyes.com/en/yogyakarta-tourism-object/other/museum-gunung-api-merapi/1.jpg" width="75px" alt="post">
+                                <div class="media-body">
+                                    <a href="https://www.yogyes.com/id/yogyakarta-tourism-object/other/museum-gunung-api-merapi/">
+                                        <h3>Museum Gunung Api Merapi</h3>
+                                    </a>
+                                    <p>03 Hours ago</p>
+                                </div>
+                            </div>
+                            <div class="media post_item">
+                                <img src="https://www.yogyes.com/en/yogyakarta-tourism-object/other/sindu-kusuma-edupark/1.jpg" width="75px" alt="post">
+                                <div class="media-body">
+                                    <a href="https://www.yogyes.com/id/yogyakarta-tourism-object/other/sindu-kusuma-edupark/">
+                                        <h3>Sindu Kusuma Edupark</h3>
+                                    </a>
+                                    <p>01 Hours ago</p>
+                                </div>
+                            </div>
+                        </aside>
+                    </div>
+                </div>
+            </div>
+            <div class="navigation-area">
+                <div class="row">
+                    <div class="col-lg-6 col-md-8 col-12 nav-left flex-row d-flex justify-content-start align-items-center">
+                        <div class="thumb">
+                            <a href="https://jejakpiknik.com/candi-abang/">
+                                <img class="img-fluid" src="https://www.jejakpiknik.com/wp-content/uploads/2017/09/@diochandra__-630x380.jpg" width="100px" alt="">
+                            </a>
+                        </div>
+                        <div class="arrow">
+                            <a href="https://jejakpiknik.com/candi-abang/">
+                                <span class="lnr text-white ti-arrow-left"></span>
+                            </a>
+                        </div>
+                        <div class="detials">
+                            <p>Prev Post</p>
+                            <a href="https://jejakpiknik.com/candi-abang/">
+                                <h4>Candi Abang</h4>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-12 nav-right flex-row d-flex justify-content-end align-items-center">
+                        <div class="detials">
+                            <p>Next Post</p>
+                            <a href="https://www.tagar.id/gua-jepang-tanpa-kisah-mistis-di-berbah-sleman">
+                                <h4>Gua Jepang</h4>
+                            </a>
+                        </div>
+                        <div class="arrow">
+                            <a href="https://www.tagar.id/gua-jepang-tanpa-kisah-mistis-di-berbah-sleman">
+                                <span class="lnr text-white ti-arrow-right"></span>
+                            </a>
+                        </div>
+                        <div class="thumb">
+                            <a href="https://www.tagar.id/gua-jepang-tanpa-kisah-mistis-di-berbah-sleman">
+                                <img class="img-fluid" src="https://www.tagar.id/Asset/uploads2019/1599486920711-gua-jepang-di-berbah-sleman-2.jpg" width="90px" alt="">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================ Blog Area end =================-->
+
+    <!-- JS here -->
+
+    <!-- All JS Custom Plugins Link Here here -->
+    <script src="./assets/js/vendor/modernizr-3.5.0.min.js"></script>
+
+    <!-- Jquery, Popper, Bootstrap -->
+    <script src="./assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="./assets/js/popper.min.js"></script>
+    <script src="./assets/js/bootstrap.min.js"></script>
+    <!-- Jquery Mobile Menu -->
+    <script src="./assets/js/jquery.slicknav.min.js"></script>
+
+    <!-- Jquery Slick , Owl-Carousel Plugins -->
+    <script src="./assets/js/owl.carousel.min.js"></script>
+    <script src="./assets/js/slick.min.js"></script>
+    <!-- Date Picker -->
+    <script src="./assets/js/gijgo.min.js"></script>
+    <!-- One Page, Animated-HeadLin -->
+    <script src="./assets/js/wow.min.js"></script>
+    <script src="./assets/js/animated.headline.js"></script>
+    <script src="./assets/js/jquery.magnific-popup.js"></script>
+
+    <!-- Scrollup, nice-select, sticky -->
+    <script src="./assets/js/jquery.scrollUp.min.js"></script>
+    <script src="./assets/js/jquery.nice-select.min.js"></script>
+    <script src="./assets/js/jquery.sticky.js"></script>
+
+    <!-- contact js -->
+    <script src="./assets/js/contact.js"></script>
+    <script src="./assets/js/jquery.form.js"></script>
+    <script src="./assets/js/jquery.validate.min.js"></script>
+    <script src="./assets/js/mail-script.js"></script>
+    <script src="./assets/js/jquery.ajaxchimp.min.js"></script>
+
+    <!-- Jquery Plugins, main Jquery -->
+    <script src="./assets/js/plugins.js"></script>
+    <script src="./assets/js/main.js"></script>
+
+
+    <!-- JS here -->
+
+    <!-- All JS Custom Plugins Link Here here -->
+    <script src="{{ asset('/dist/js/modernizr-3.5.0.min.js') }}"></script>
+
+    <!-- Jquery, Popper, Bootstrap -->
+    <script src="{{ asset('assets/libs/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/popper.js/dist/umd/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <!-- Jquery Mobile Menu -->
+    <script src="{{ asset('/dist/js/jquery.slicknav.min.js') }}"></script>
+
+    <!-- Jquery Slick , Owl-Carousel Plugins -->
+    <script src="{{ asset('/dist/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('/dist/js/slick.min.js') }}"></script>
+    <!-- Date Picker -->
+    {{-- <script src="./assets/js/gijgo.min.js"></script> --}}
+    <!-- One Page, Animated-HeadLin -->
+    <script src="{{ asset('/dist/js/wow.min.js') }}"></script>
+    <script src="{{ asset('/dist/js/animated.headline.js') }}"></script>
+    <script src="{{ asset('/dist/js/jquery.magnific-popup.js') }}"></script>
+
+    <!-- Scrollup, nice-select, sticky -->
+    <script src="{{ asset('dist/js/jquery.scrollUp.min.js') }}"></script>
+    <script src="{{ asset('dist/js/jquery.nice-select.min.js') }}"></script>
+    <script src="{{ asset('dist/js/jquery.sticky.js') }}"></script>
+
+    <!-- contact js -->
+    {{-- <script src="./assets/js/contact.js"></script>
+    <script src="./assets/js/jquery.form.js"></script>
+    <script src="./assets/js/jquery.validate.min.js"></script>
+    <script src="./assets/js/mail-script.js"></script>
+    <script src="./assets/js/jquery.ajaxchimp.min.js"></script> --}}
+
+    <!-- Jquery Plugins, main Jquery -->
+    <script src="{{ asset('/dist/js/plugins.js') }}"></script>
+    <script src="{{ asset('dist/js/landing.js') }}"></script>
+
+</body>
+
+</html>
