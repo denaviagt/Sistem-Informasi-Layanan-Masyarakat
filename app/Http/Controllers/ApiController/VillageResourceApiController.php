2c80d9e912b02e93669c943430b429f0eae41efb
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-02-06 06:46:34 +0700

    list dan detail potensi desa

--- app/Http/Controllers/ApiController/VillageResourceApiController.php
+++ app/Http/Controllers/ApiController/VillageResourceApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\ApiController;
 
+use App\Models\NaturalResource;
 use Illuminate\Http\JsonResponse;
 
 class VillageResourceApiController extends ApiController
@@ -13,9 +14,10 @@ class VillageResourceApiController extends ApiController
      */
     public function index()
     {
-        return response()->json([
-            'data' => []
-        ]);
+        $data = NaturalResource::all();
+        $message = "List of Village's Natural Resource";
+
+        return $this->successResponse(compact('data', 'message'));
     }
 
     /**
@@ -26,8 +28,15 @@ class VillageResourceApiController extends ApiController
      */
     public function show($id)
     {
-        return response()->json([
-            'message' => 'Village Resource ' . $id
-        ]);
+        $data = NaturalResource::find($id);
+        $title = $data != null ? $data->title : "";
+        $message = "Detail Village's Natural Resource" . $title;
+
+        if ($data == null) {
+            $message = "Village's Natural Resource not found";
+            return $this->errorResponse(compact('message'));
+        }
+
+        return $this->successResponse(compact('data', 'message'));
     }
 }
