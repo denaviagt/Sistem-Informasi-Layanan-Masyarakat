654c94973c62c7adbe235397b22e7b819bd9ded1
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-02-06 09:14:18 +0700

    submit feedback

--- app/Http/Controllers/ApiController/FeedbackApiController.php
+++ app/Http/Controllers/ApiController/FeedbackApiController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\ApiController;
 
+use App\Models\Dusun;
+use App\Models\Feedback;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -26,16 +28,53 @@ class FeedbackApiController extends ApiController
      */
     public function store(Request $request)
     {
-        return response()->json([
-            'message' => 'Feedback ' . $request,
-            'data' => []
-        ], 201);
+        $isImportant = $request->is_important;
+        $isAnonim = $request->is_anonim;
+        $isRead = $request->is_read;
+        $feedback = $request->feedback;
+        $date = $request->date ?? now();
+        $userId = $request->user_id;
+        $dusunWhere = $request->feedback_dusun_id != null ? "id" : "dusun_name";
+        $dusunClause = $request->feedback_dusun_id ?? $request->dusun_name;
+        $feedbackDusunId = null;
+
+        if ($dusunWhere == null) {
+            $message = "Field dusun_name or feedback_dusun_id required";
+            return $this->errorResponse(compact('message'));
+        }
+
+        if (($dusun = Dusun::where($dusunWhere, $dusunClause)->first()) == null) {
+            $message = "Dusun Not Found!";
+            return $this->errorResponse(compact('message'), 404);
+        }
+        $feedbackDusunId = $dusun->id;
+
+        $feedback = Feedback::create(
+            compact(
+                'isImportant',
+                'isAnonim',
+                'isRead',
+                'feedback',
+                'date',
+                'userId',
+                'feedbackDusunId'
+            ));
+
+        if (!$feedback) {
+            $message = "Gagal Mengirimkan Aduan!";
+            return $this->errorResponse(compact('message'), 404);
+        }
+
+        $data = $feedback->procedure;
+        $message = "Berhasil Mengirimkan Aduan!";
+
+        return $this->successResponse(compact('data', 'message'), 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return Response
      */
     public function show($id)
@@ -47,7 +86,7 @@ class FeedbackApiController extends ApiController
      * Update the specified resource in storage.
      *
      * @param Request $request
-     * @param  int  $id
+     * @param int $id
      * @return Response
      */
     public function update(Request $request, $id)
@@ -58,7 +97,7 @@ class FeedbackApiController extends ApiController
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return Response
      */
     public function destroy($id)
