2d96b8a63ce749020b792f44108794fef6d638aa
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-02-06 20:10:22 +0700

    register user

--- app/Http/Controllers/ApiController/AuthApiController.php
+++ app/Http/Controllers/ApiController/AuthApiController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers\ApiController;
 
 use App\Http\Requests\LoginApiRequest;
 use App\Http\Resources\UserResource;
+use App\Models\Citizen;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 
 class AuthApiController extends ApiController
 {
@@ -37,26 +37,45 @@ class AuthApiController extends ApiController
     public function register(Request $request)
     {
         $this->validate($request, [
-            'username' => ['required', 'min:5'],
+            'nik' => ['required', 'min:16'],
+            'phone_number' => ['required'],
             'password' => ['required', 'min:8'],
         ]);
 
-        $user = new User();
-        $user->username = $request->username;
-        $user->password = Hash::make($request->password);
-//        $user->remember_token = Str::random(60);
+        $nik = $request->nik;
+        $password = $request->password;
+        $phone = $request->phone_number;
 
-        if ($user->save()) {
-            dd($user);
-            return (new UserResource($user))
-                ->additional([
-                    'meta' => [
-//                    'token' => $user->remember_token
-                    ]
-                ]);
-        } else {
+        $citizen = Citizen::where('nik', $nik)->first();
+
+        if ($citizen == null) {
+            $message = "Data Penduduk Tidak Ditemukan!";
+            return $this->errorResponse(compact('message'), 404);
+        }
+
+        $username = $this->generateUsername($citizen->full_name);
+        $email = "";
+        $citizen_id = $citizen->id;
+
+        $user = User::create(compact('username', 'email', 'citizen_id', 'password', 'phone'));
+
+        if ($user == null) {
             dd($user->getRelations());
         }
+
+        return $this->successResponse(new UserResource($user), 201);
+    }
+
+    /**
+     * @param string $word
+     * @param string $separator
+     * @return string
+     */
+    private function generateUsername(string $word, string $separator = '.'): string
+    {
+        $words = explode(' ', $this->string_filter(trim($word), " "));
+        $username = implode($separator, [$words[0], $words[1]]);
+        return strtolower($username);
     }
 
 }
