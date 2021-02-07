edd0f8397470d892eb485b2511bb31562879410e
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-02-07 07:32:34 +0700

    perbaikan response strucktur organisasi

--- app/Http/Resources/OrganizationResource.php
+++ app/Http/Resources/OrganizationResource.php
@@ -24,8 +24,32 @@ class OrganizationResource extends JsonResource
                 "citizen_id" => $this->citizen_id,
                 "admin_id" => $this->admin_id
             ],
-            'citizen' => $this->citizen,
-            'admin' => $this->admin,
+            'citizen' => [
+                "id" => $this->citizen->id,
+                "nik" => $this->citizen->nik,
+                "kk" => $this->citizen->kk,
+                "full_name" => $this->citizen->full_name,
+                "gender" => $this->citizen->gender,
+                "religion" => $this->citizen->religion,
+                "married_status" => $this->citizen->married_status,
+                "last_education" => $this->citizen->last_education,
+                "blood_type" => $this->citizen->blood_type,
+                "profession" => $this->citizen->profession,
+                "pob" => $this->citizen->pob,
+                "dob" => $this->citizen->dob,
+                "address" => $this->citizen->address,
+                "status" => $this->citizen->status,
+                "dusun_id" => $this->citizen->dusun_id,
+            ],
+            'admin' => [
+                "id" => $this->admin->id,
+                "full_name" => $this->admin->full_name,
+                "gender" => $this->admin->gender,
+                "email" => $this->admin->email,
+                "username" => $this->admin->username,
+                "is_active" => boolval($this->admin->is_active),
+                "level" => $this->admin->level,
+            ],
         ];
     }
 }
