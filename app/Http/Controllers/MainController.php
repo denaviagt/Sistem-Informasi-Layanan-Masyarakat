ec92031bb06b4fe5b3d01f3877a7a8357ad0ba12
Anggita Denavia <anggitadenav@gmail.com>
2021-12-22 19:10:13 +0700

    Front

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Admin;
 use App\Models\Apparatus;
+use App\Models\Dusun;
 use App\Models\Mission;
 use App\Models\NaturalResource;
 use App\Models\NaturalResourceImage;
+use App\Models\Service;
 use App\Models\ServiceCategory;
 use App\Models\ServiceProcedure;
 use App\Models\ServiceRequirement;
@@ -19,37 +21,42 @@ class MainController extends Controller
 {
     public function index()
     {
-        $mission = Mission::all();
-        $vision = Vision::all();
-        $apparatus = Apparatus::all();
-        $villageInfos = VillageInfo::limit(6)->orderBy('date', 'desc')->where('status', 'published')->get();
-        $naturalResources = NaturalResource::limit(6)->get();
-        $naturalResourceImage = array();
-        foreach ($naturalResources as $key => $value) {
-            $naturalResourceDate = $naturalResources[$key]->created_at->isoFormat('dddd, D MMMM Y');
-            $naturalResourceImageTemp = NaturalResourceImage::where('natural_resource_id', $naturalResources[$key]->id)->first();
-            array_push($naturalResourceImage, $naturalResourceImageTemp);
-        }
+        // $mission = Mission::all();
+        // $vision = Vision::all();
+        // $apparatus = Apparatus::all();
+        // $villageInfos = VillageInfo::limit(6)->orderBy('date', 'desc')->where('status', 'published')->get();
+        // $naturalResources = NaturalResource::limit(6)->get();
+        // $naturalResourceImage = array();
+        // foreach ($naturalResources as $key => $value) {
+        //     $naturalResourceDate = $naturalResources[$key]->created_at->isoFormat('dddd, D MMMM Y');
+        //     $naturalResourceImageTemp = NaturalResourceImage::where('natural_resource_id', $naturalResources[$key]->id)->first();
+        //     array_push($naturalResourceImage, $naturalResourceImageTemp);
+        // }
         $serviceCategories = ServiceCategory::all();
         // return $naturalResourceImage;
 
-        return view('main', compact(['villageInfos', 'naturalResources', 'naturalResourceDate', 'naturalResourceImage', 'vision', 'mission', 'apparatus', 'serviceCategories']));
-    }
-    public function villageInfoDetail($id)
-    {
-        $villageInfos = VillageInfo::limit(6)->orderBy('date', 'desc')->where('status', 'published')->get();
-        $villageInfo = VillageInfo::find($id);
-        $admin = Admin::find($villageInfo->admin_id);
-
-        return view('view-info', compact('villageInfo', 'admin', 'villageInfos'));
+        // return view('main', compact(['villageInfos', 'naturalResources', 'naturalResourceDate', 'naturalResourceImage', 'vision', 'mission', 'apparatus', 'serviceCategories']));
+        return view('front.layanan.index', compact('serviceCategories'));
     }
 
-    public function ServiceInfoDetail($id)
+    public function show($id)
     {
-        $procedures = ServiceProcedure::where('service_category_id', $id)->get();
-        $requirements = ServiceRequirement::where('service_category_id', $id)->get();
-        $serviceCategories = ServiceCategory::all();
-        $judul = ServiceCategory::where('id', $id)->first();
-        return view('service-info-detail', compact('procedures', 'requirements', 'serviceCategories', 'judul'));
+        $service_category = ServiceCategory::find($id);
+
+        return view('front.layanan.show', compact('service_category', 'id'));
+    }
+
+    public function create($id)
+    {
+        $service_category = ServiceCategory::find($id);
+
+        return view('front.layanan.create', compact('service_category', 'id'));
+    }
+
+    public function aduan_create()
+    {
+        $dusun = Dusun::all();
+
+        return view('front.aduan.create', compact('dusun'));
     }
 }
