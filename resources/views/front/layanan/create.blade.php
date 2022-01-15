a0b1993208ca7f44e47f295d2976b3a579bb73a1
Anggita Denavia <anggitadenav@gmail.com>
2022-01-15 17:36:39 +0700

    Create Layanan

--- resources/views/front/layanan/create.blade.php
+++ resources/views/front/layanan/create.blade.php
@@ -8,22 +8,78 @@
             </div>
             <div>
                 <form class="form-contact">
-                    <div class="form-row">
+
+                    <div class="switch-wrap d-flex ">
+                        <div class="primary-switch mr-3">
+                            <input type="checkbox" id="other">
+                            <label for="other"></label>
+                        </div>
+                        <p for="other">Ajukan untuk orang lain</p>
+                    </div>
+
+                    <div id="body-bio">
+
+                    </div>
+
+                    @foreach ($service_category->requirement as $pc)
+                        <div>
+                            <label for="alamat">{{ $pc->terms }}</label>
+                            <div class="custom-file mb-5">
+                                <input type="file" class="custom-file-input" id="file_{{ $pc->id }}">
+                                <label class="custom-file-label" for="file_{{ $pc->id }}">Unggah File</label>
+                            </div>
+                        </div>
+                    @endforeach
+
+                    <div class="d-flex justify-content-center">
+
+                        <button type="submit" class="btn btn-primary mx-auto">Ajukan Permohonan</button>
+                    </div>
+                </form>
+            </div>
+
+        </div>
+
+
+    </main>
+@endsection
+
+@section('js')
+    <script>
+        $('#other').on('change', function() {
+            let value = $(this).val();
+            let result = ` <div class="form-row">
                         <div class="form-group col-md-6">
                             <label for="nik">NIK</label>
-                            <input type="text" name="nik" placeholder="Masukkan NIK" class="form-control single-input"
-                                id="nik">
+                            <input type="text" name="nik" placeholder="Masukkan NIK"
+                                class="form-control single-input @error('nik') is-invalid @enderror" id="nik">
+                            @error('nik')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
                         <div class="form-group col-md-6">
                             <label for="kk">Nomor KK</label>
-                            <input type="text" name="kk" placeholder="Masukkan No KK" class="form-control" id="kk">
+                            <input type="text" name="kk" placeholder="Masukkan No KK"
+                                class="form-control @error('kk') is-invalid @enderror" id="kk">
+                            @error('kk')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
                     </div>
                     <div class="form-row">
                         <div class="form-group col-md-6">
                             <label for="nama">Nama</label>
-                            <input type="text" name="name" placeholder="Masukkan Nama Lengkap" class="form-control"
-                                id="nama">
+                            <input type="text" name="name" placeholder="Masukkan Nama Lengkap"
+                                class="form-control @error('name') is-invalid @enderror" id="nama">
+                            @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
                         <div class="form-group col-md-6">
                             <label>Jenis Kelamin</label>
@@ -73,26 +129,13 @@
                         <label for="alamat">Alamat</label>
                         <textarea class="form-control w-100" name="alamat" id="alamat" cols="30" rows="4"
                             placeholder="Masukkan Alamat"></textarea>
-                    </div>
-                    @foreach ($service_category->requirement as $pc)
-                        <div>
-                            <label for="alamat">{{ $pc->terms }}</label>
-                            <div class="custom-file mb-5">
-                                <input type="file" class="custom-file-input" id="customFile">
-                                <label class="custom-file-label" for="customFile">Unggah File</label>
-                            </div>
-                        </div>
-                    @endforeach
-
-                    <div class="d-flex justify-content-center">
-
-                        <button type="submit" class="btn btn-primary mx-auto">Ajukan Permohonan</button>
-                    </div>
-                </form>
-            </div>
-
-        </div>
-
-
-    </main>
+                    </div>`
+            if (value == 'on') {
+                $('#body-bio').html(result)
+            } else {
+                console.log('assd');
+                $('#body-bio').empty()
+            }
+        })
+    </script>
 @endsection
